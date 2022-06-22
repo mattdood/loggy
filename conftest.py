d49d9d31f2f4214edfa6be4cc54a226f86e4502c
mattdood <mawimberly@csumb.edu>
2022-06-21 20:13:23 -0700

    Loggy has tests + a custom formatter test

--- conftest.py
+++ conftest.py
@@ -1,2 +1,15 @@
 import pytest
+from typing import Dict
+
+from loggy.loggy import LogFormatter
+
+
+class CustomLogFormatter(LogFormatter):
+    """Adds a custom color to a level as an example."""
+
+    def __init__(self, *args, color_dict: Dict = None, **kwargs) -> None:
+        super().__init__()
+        if color_dict:
+            self.COLORS.update(color_dict)
+
 
