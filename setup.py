465639b0444ac01f172faa156cee04fed5a41e01
mattdood <mawimberly@csumb.edu>
2022-06-21 20:36:44 -0700

    Upversion

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="get-loggy",
-    version="0.0.1",
+    version="0.0.2",
     author="Matthew Wimberly",
     author_email="matthew.wimb@gmail.com",
     description="A simple logging utility.",
